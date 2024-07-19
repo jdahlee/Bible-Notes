a5e93e7875bc1f7ca5a653caf977a48d2bda9f3a
jdahlee <judahlee29@gmail.com>
2024-07-18 22:21:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# To activate venv run
+
+activate
+
+With bash run
+
+source .venv/Scripts/activate
+
+# To deactivate venv run
+
+deactivate
+
+# To run the backend server locally
+
+flask --debug run -h localhost
+
+make sure you're in the backend directory
+
+# To update the requirements.txt run
+
+pip freeze > requirements.txt
+
+# To run front end server
+
+npm run dev
---
+++ frontend/src/UserList.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const UserList = ({ users, updateUser, updateCallback }) => {
+  const onDelete = async (id) => {
+    try {
+      const options = {
+        method: "DELETE",
+      };
+      const response = await fetch(
+        `http://localhost:5000/delete_user/${id}`,
+        options
+      );
+      if (response.status === 200) {
+        updateCallback();
+      } else {
+        console.error("Failed to delte");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+  return (
+    <div>
+      {JSON.stringify(users)}
+      <h2>Users</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.email}</td>
+              <td>
+                <button onClick={() => updateUser(user)}>Update</button>
+                <button onClick={() => onDelete(user.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserList;
