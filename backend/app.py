72f8c0c63240a44ef433a3e494f54367aad1f856
jdahlee <judahlee29@gmail.com>
2024-08-12 09:10:13 -0500

    Create backend update and delete note functions

--- backend/app.py
+++ backend/app.py
@@ -35,6 +35,35 @@ def create_note():
     
     return jsonify({"message": "Note created", "note": new_note.to_json()}), 201
 
+@app.route("/update_note/<int:note_id>", methods=["PATCH"])
+def update_note(note_id):
+    note = Note.query.get(note_id)
+
+    if not note:
+        return jsonify({"message": "Note not found"}), 404
+    
+    data = request.json
+    note.title = data.get("title", note.title)
+    note.body = data.get("body", note.body)
+    note.source = data.get("source", note.source)
+    note.tags = data.get("tags", note.tags)
+
+    db.session.commit()
+
+    return jsonify({"message": "Note updated." }), 200
+
+@app.route("/delete_note/<int:note_id>", methods=["DELETE"])
+def delete_note(note_id):
+    note = Note.query.get(note_id)
+
+    if not note:
+        return jsonify({"message": "Note not found"}), 404
+    
+    db.session.delete(note)
+    db.session.commit()
+
+    return jsonify({"message": "Note deleted!"}), 200
+
 @app.route("/users", methods=["GET"])
 def get_users():
     users = User.query.all()
