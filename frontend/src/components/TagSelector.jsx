6e7632e0547ca9ff218c2f53f518d8e6844b75bf
jdahlee <judahlee29@gmail.com>
2024-08-15 15:59:49 -0500

    Front Create TagSelector component

---
+++ frontend/src/components/TagSelector.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const TagSelector = ({ tagOptions, currentTag, onTagChange }) => {
+  console.log("currentTag", currentTag);
+  return (
+    <div className="p-4">
+      <select
+        value={currentTag}
+        onChange={(e) => onTagChange(e.target.value)}
+        className="block w-full p-2 border border-gray-300 rounded-md"
+      >
+        <option value="" disabled hidden>
+          {currentTag || "Select a tag"}
+        </option>
+        {tagOptions.map((tag) => (
+          <option key={tag} value={tag}>
+            {tag}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default TagSelector;
